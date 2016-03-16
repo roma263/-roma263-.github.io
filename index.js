473ea1a531f29b82553d4a085ca31495702b81ee
roma263 <roma@romas-MacBook-Pro.local>
2016-03-16 19:55:00 -0400

    wooo
    woo

---
+++ index.js
@@ -0,0 +1,13 @@
+function my_first_js_function() {
+
+}
+
+function kosbie_face_click_handler(){
+    alert("ouch");
+
+}
+function on_page_ready() {
+    $(".kosbiesface").click(kosbie_face_click_handler)
+}
+$(document).ready(on_page_ready)
+
